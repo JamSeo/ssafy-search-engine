e88baa4732d783b641881676a19d6bc0b997c997
이다곤 <leedagon624@gmail.com>
2023-10-26 14:32:36 +0900

    ⚡️ feat: api에 이미지 s3 업로드 기능 추가

--- Backend/src/main/java/com/ssafy/sse/controller/FileController.java
+++ Backend/src/main/java/com/ssafy/sse/controller/FileController.java
@@ -1,5 +1,7 @@
 package com.ssafy.sse.controller;
 
+import java.io.IOException;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,11 +9,14 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.ssafy.sse.dto.FileDto;
 import com.ssafy.sse.entity.File;
 import com.ssafy.sse.service.FileService;
+import com.ssafy.sse.service.S3UploadService;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -23,10 +28,15 @@ import lombok.extern.slf4j.Slf4j;
 @Configuration
 public class FileController {
 	private final FileService fileService;
+	private final S3UploadService s3UploadService;
 
 	@PostMapping
-	public ResponseEntity create(@RequestBody FileDto fileDto) {
+	public ResponseEntity create(@RequestPart(value="dto") FileDto fileDto,
+								@RequestPart(value="image") MultipartFile image) throws IOException {
+
 		log.info("create file");
+		String res = s3UploadService.saveFile(image);
+		System.out.println(res);
 		File file = fileService.create(fileDto);
 		return ResponseEntity.ok(file);
 	}
