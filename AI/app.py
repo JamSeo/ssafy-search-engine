9ed2537398071a31a409966315fcde268d418f62
김의준 <euijun98@naver.com>
2023-10-27 11:25:15 +0900

    :sparkles:feat: flask ocr 적용

--- AI/app.py
+++ AI/app.py
@@ -1,10 +1,23 @@
 from flask import Flask
+import easyocr
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return 'Hello, World'
+
+@app.route('/ai/ocr')
+def ocr():
+    path = "test.png"
+    reader = easyocr.Reader(['ko', 'en'])
+    result = reader.readtext(path, detail = 0)
+    output = ""
+    for x in result:
+        output += (x + " ")
+
+    output.replace('    ','  ')
+    return output
 
 if __name__ == '__main__':
     app.run(debug=True)
